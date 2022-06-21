f76c679d0a026fe5e20c5cff9bf1cade176efa67
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-06-21 09:46:17 -0300

    Demais métodos de busca implementados e testados

--- Geen_Exercicio4/Program.cs
+++ Geen_Exercicio4/Program.cs
@@ -17,7 +17,11 @@ namespace Geen_Exercicio4
                 new Linguagem{Ano = 1995, Nome = "Ruby", DesenvolvedorChefe = "Yukihiro Matsumoto", Predecessores = "Smalltalk, Perl"},
                 new Linguagem{Ano = 2009, Nome = "Go", DesenvolvedorChefe = "Google", Predecessores = "C; Oberon, Limbo; Smalltalk"},
             };
-            BuscarLinguagemPorNome(listaDeLinguagens, "Ruby");
+
+            BuscarLinguagemPorNome(listaDeLinguagens, "AMOS");
+            BuscarLinguagemPorAno(listaDeLinguagens, 1991);
+            BuscarLinguagemPorDesenvolvedor(listaDeLinguagens, "Yuki");
+            BuscarLinguagemPorPredecessor(listaDeLinguagens, "Oberon");
         }
 
         static void AdicionarLinguagem(List<Linguagem> listaDeLinguagens)
@@ -43,6 +47,51 @@ namespace Geen_Exercicio4
                         select linguagem;
 
             foreach(var linguagem in dados)
+            {
+                Console.WriteLine($"Nome: {linguagem.Nome} \n" +
+                                  $"Ano de criação: {linguagem.Ano} \n" +
+                                  $"Desenvolvedor Chefe: {linguagem.DesenvolvedorChefe} \n" +
+                                  $"Predecessores: {linguagem.Predecessores}");
+                Console.WriteLine();
+            }
+        }
+        static void BuscarLinguagemPorAno(List<Linguagem> listaDeLinguagens, int ano)
+        {
+            var dados = from linguagem in listaDeLinguagens
+                        where linguagem.Ano.Equals(ano)
+                        select linguagem;
+
+            foreach (var linguagem in dados)
+            {
+                Console.WriteLine($"Nome: {linguagem.Nome} \n" +
+                                  $"Ano de criação: {linguagem.Ano} \n" +
+                                  $"Desenvolvedor Chefe: {linguagem.DesenvolvedorChefe} \n" +
+                                  $"Predecessores: {linguagem.Predecessores}");
+                Console.WriteLine();
+            }
+        }
+        static void BuscarLinguagemPorDesenvolvedor(List<Linguagem> listaDeLinguagens, string desenvolvedor)
+        {
+            var dados = from linguagem in listaDeLinguagens
+                        where linguagem.DesenvolvedorChefe.Contains(desenvolvedor)
+                        select linguagem;
+
+            foreach (var linguagem in dados)
+            {
+                Console.WriteLine($"Nome: {linguagem.Nome} \n" +
+                                  $"Ano de criação: {linguagem.Ano} \n" +
+                                  $"Desenvolvedor Chefe: {linguagem.DesenvolvedorChefe} \n" +
+                                  $"Predecessores: {linguagem.Predecessores}");
+                Console.WriteLine();
+            }
+        }
+        static void BuscarLinguagemPorPredecessor(List<Linguagem> listaDeLinguagens, string predecessor)
+        {
+            var dados = from linguagem in listaDeLinguagens
+                        where linguagem.Predecessores.Contains(predecessor)
+                        select linguagem;
+
+            foreach (var linguagem in dados)
             {
                 Console.WriteLine($"Nome: {linguagem.Nome} \n" +
                                   $"Ano de criação: {linguagem.Ano} \n" +
